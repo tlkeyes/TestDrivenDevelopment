324a80452be1fac787919483724d0bfce3f8e8f6
lkeyes <lkeyes@tha.com>
2018-04-20 08:39:52 -0500

    First Unit Test and URL Mapping, Dummy View

--- Superlists/urls.py
+++ Superlists/urls.py
@@ -3,6 +3,7 @@ Definition of urls for Superlists.
 """
 
 from django.conf.urls import include, url
+from lists import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,6 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', views.home_page, name='home'),
 ]
