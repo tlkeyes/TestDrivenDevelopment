01ae3efb7f253225d68fd2d074add14bbff30b97
lkeyes <lkeyes@tha.com>
2018-04-20 13:35:20 -0500

    Front Page Html now Generated From Template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         #   She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id_('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -38,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(rows.text == '1: Buy peacock feathers' for row in rows)
+            any(rows.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
             )
 
         #   There is still a text box inviting her to add another item. She
